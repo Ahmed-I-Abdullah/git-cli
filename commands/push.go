96c9338d8e295103856b0bf8d8af5aa2f7552ae8
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2024-04-05 18:10:06 -0600

    fix: fix cli push

--- commands/push.go
+++ commands/push.go
@@ -1,14 +1,17 @@
 package commands
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/Ahmed-I-Abdullah/p2p-code-collaboration/pb"
 	"github.com/ammar-y62/git-cli/git"
 	"github.com/ammar-y62/git-cli/grpc"
 	"github.com/urfave/cli/v2"
+	google_grpc "google.golang.org/grpc"
 )
 
 func pushViaGRPC(c *cli.Context) error {
@@ -49,7 +52,18 @@ func pushViaGRPC(c *cli.Context) error {
 		return fmt.Errorf("Failed to push repository to leader: %v", err)
 	}
 
-	notifyResponse, err := grpcClient.NotifyPushCompletion(ctx, &pb.NotifyPushCompletionRequest{Name: repoName})
+	grpcCtx, grpcCancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer grpcCancel()
+	leaderConn, err := google_grpc.DialContext(grpcCtx, response.GrpcAddress, google_grpc.WithInsecure(), google_grpc.WithBlock())
+
+	if err != nil {
+		return fmt.Errorf("Failed to connect to leader with gRPC: %v", err)
+	}
+	defer leaderConn.Close()
+
+	leaderGrpcClient := pb.NewRepositoryClient(leaderConn)
+
+	notifyResponse, err := leaderGrpcClient.NotifyPushCompletion(ctx, &pb.NotifyPushCompletionRequest{Name: repoName})
 
 	if err != nil {
 		return fmt.Errorf("Failed to notify leader about repository push: %v", err)
